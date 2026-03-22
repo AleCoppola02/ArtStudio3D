e686f18da95a1955b58af1c3effc6ac3090e51fb
AleCoppola02 <a.coppola197@studenti.unisa.it>
2026-03-22 17:02:21 +0100

    Fixed minor issues with blend modes and scene setup. Started adding experimental tile-based canvas architecture

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -1,10 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CanvasManager : MonoBehaviour
 {
     public RenderTexture canvasRT;
-
     private Color canvasColor = new Vector4(1, 1, 1, 1);
+
+
+    // Using a dictionary to look up tiles using their grid coordinates
+    public Dictionary<Vector2Int, CanvasTile> tiles = new Dictionary<Vector2Int, CanvasTile>();
+
+    public int tileSize = 256; //tile size in pixels
+
+    public CanvasTile GetOrCreateTile(Vector2Int gridPos) {
+        if (!tiles.ContainsKey(gridPos)) {
+            CanvasTile newTile = new CanvasTile();
+            newTile.gridPosition = gridPos;
+            newTile.pixelBounds = new Rect(gridPos.x * tileSize, gridPos.y * tileSize, tileSize, tileSize);
+            tiles.Add(gridPos, newTile);
+        }
+        return tiles[gridPos];
+    }
+
     public RenderTexture GetCanvasRT() {
         return canvasRT;
     }
@@ -18,4 +35,8 @@ public class CanvasManager : MonoBehaviour
         GL.Clear(true, true, canvasColor);
         RenderTexture.active = null;
     }
+
+
+
+
 }
